240c0399f34c0f2dce9da9761657ec2f3eb0771a
Hiroyoshi Houchi <git@hixi-hyi.com>
2021-11-04 10:38:21 +0900

    support_nexttoken

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/hixi-hyi/ssm-bypass
+
+go 1.17
+
+require github.com/aws/aws-sdk-go v1.41.17
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- main.go
+++ main.go
@@ -10,24 +10,44 @@ import (
 	"github.com/aws/aws-sdk-go/service/ssm"
 )
 
+type Parameter struct {
+	Name  string
+	Value string
+}
+
 func main() {
-    flag.Parse()
-    args := flag.Args()
-    sess, err := session.NewSession()
-    if err != nil {
-        panic(err)
-    }
-    svc := ssm.New(sess)
-    res, err := svc.GetParametersByPath(&ssm.GetParametersByPathInput{
-        Path: aws.String(args[0]),
-        WithDecryption: aws.Bool(true),
-    })
-    if err != nil {
-        panic(err)
-    }
-    for _, v := range res.Parameters {
-        arr := strings.Split(*v.Name, "/")
-        name := arr[len(arr)-1]
-        fmt.Printf("export %s=%s\n", name, *v.Value)
-    }
+	flag.Parse()
+	args := flag.Args()
+	sess, err := session.NewSession()
+	if err != nil {
+		panic(err)
+	}
+	svc := ssm.New(sess)
+	var parameters []Parameter
+	nextToken := ""
+	for {
+		req := &ssm.GetParametersByPathInput{
+			Path:           aws.String(args[0]),
+			WithDecryption: aws.Bool(true),
+		}
+		if nextToken != "" {
+			req.NextToken = aws.String(nextToken)
+		}
+		res, err := svc.GetParametersByPath(req)
+		if err != nil {
+			panic(err)
+		}
+		for _, v := range res.Parameters {
+			arr := strings.Split(*v.Name, "/")
+			name := arr[len(arr)-1]
+			parameters = append(parameters, Parameter{Name: name, Value: *v.Value})
+		}
+		if res.NextToken == nil {
+			break
+		}
+		nextToken = *res.NextToken
+	}
+	for _, v := range parameters {
+		fmt.Printf("export %s='%s'\n", v.Name, v.Value)
+	}
 }
